36c6e7cbfe7d49f49c1d9aa68be64e150b8e0b31
arif <arifmr91@gmail.com>
2016-12-13 10:41:01 +0530

    html changed

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,14 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/',ensureAuthenticated,function (req,res) {
+router.get('/', ensureAuthenticated, function (req, res) {
     res.render('index');
 });
 
-function ensureAuthenticated(req,res,next) {
-    if(req.isAuthenticated()){
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
         return next();
-    }else{
+    } else {
         //req.flash('error_msg','you are not logged in');
         res.redirect('/users/login');
     }
